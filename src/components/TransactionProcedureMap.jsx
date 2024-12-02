edbb1d2dc244472b58ad0ffbff9eae73b7b252d3
Programmerdin <testingemailodin@gmail.com>
2024-12-02 01:00:00 -0800

    adding procedure0027 despite not being done

--- src/components/TransactionProcedureMap.jsx
+++ src/components/TransactionProcedureMap.jsx
@@ -22,7 +22,7 @@ import Procedure0022 from "../transaction_procedures/0022/Procedure0022";
 import Procedure0024 from "../transaction_procedures/0024/Procedure0024";
 import Procedure0025 from "../transaction_procedures/0025/Procedure0025";
 import Procedure0026 from "../transaction_procedures/0026/Procedure0026";
-// import Procedure0027 from "../transaction_procedures/0027/Procedure0027";
+import Procedure0027 from "../transaction_procedures/0027/Procedure0027";
 import Procedure0028 from "../transaction_procedures/0028/Procedure0028";
 
 const TransactionProcedureMap = {
@@ -50,7 +50,7 @@ const TransactionProcedureMap = {
   24: Procedure0024,
   25: Procedure0025,
   26: Procedure0026,
-  // 27: Procedure0027,
+  27: Procedure0027,
   28: Procedure0028,
 };
 
